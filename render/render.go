5f343eaa5f0abe73047c6d6041d0b7ea1a0d6802
kaliadmen <kaliadmen@gmail.com>
2022-04-05 21:51:48 -0400

    ADDS JET TO RENDER PACKAGE

--- render/render.go
+++ render/render.go
@@ -2,17 +2,20 @@ package render
 
 import (
 	"fmt"
+	"github.com/CloudyKit/jet/v6"
 	"html/template"
+	"log"
 	"net/http"
 	"strings"
 )
 
 type Render struct {
-	Renderer   string
-	RootPath   string
-	Secure     bool
-	Port       string
-	ServerName string
+	Renderer    string
+	JetTemplate *jet.Set
+	RootPath    string
+	Secure      bool
+	Port        string
+	ServerName  string
 }
 
 type TemplateData struct {
@@ -36,12 +39,17 @@ func (ren *Render) Page(w http.ResponseWriter, r *http.Request, view string, var
 			return err
 		}
 	case "jet":
+
+		err := ren.UseJet(w, r, view, variables, data)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-//UseGo uses go template engine to render template page
+//UseGo uses go template engine to render template pages
 func (ren *Render) UseGo(w http.ResponseWriter, r *http.Request, view string, data interface{}) error {
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/views/%s.page.tmpl", ren.RootPath, view))
 	if err != nil {
@@ -59,4 +67,36 @@ func (ren *Render) UseGo(w http.ResponseWriter, r *http.Request, view string, da
 	}
 
 	return nil
+}
+
+//UseJet uses jet engine to render template pages
+func (ren *Render) UseJet(w http.ResponseWriter, r *http.Request, templateName string, variables, data interface{}) error {
+	var vars jet.VarMap
+
+	//format variables for jet
+	if variables == nil {
+		vars = make(jet.VarMap)
+	} else {
+		vars = variables.(jet.VarMap)
+	}
+
+	//format template data
+	td := &TemplateData{}
+	if data != nil {
+		td = data.(*TemplateData)
+	}
+
+	t, err := ren.JetTemplate.GetTemplate(fmt.Sprintf("%s.jet", templateName))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	if err = t.Execute(w, vars, td); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+
 }
