d1c806c9cbfd4a5e1de55ea46ef8f5861a27aa82
kaliadmen <kaliadmen@gmail.com>
2022-04-20 13:34:56 -0400

    ADDS MAKE COMMAND LOGIC

---
+++ cmd/cli/make.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+func makeIt(arg2, arg3 string) error {
+	switch strings.ToLower(arg2) {
+	case "migration":
+		dbType := ds.Db.DatabaseType
+		if arg3 == "" {
+			gracefulExit(errors.New("migration must have a name"))
+		}
+
+		filename := fmt.Sprintf("%d_%s", time.Now().UnixNano(), arg3)
+
+		upFile := ds.RootPath + "/migrations/" + filename + "." + dbType + ".up.sql"
+		downFile := ds.RootPath + "/migrations/" + filename + "." + dbType + ".down.sql"
+
+		err := makeFileFromTemplate("templates/migrations/migration."+dbType+".up.sql", upFile)
+		if err != nil {
+			gracefulExit(err)
+		}
+
+		err = makeFileFromTemplate("templates/migrations/migration."+dbType+".down.sql", downFile)
+		if err != nil {
+			gracefulExit(err)
+		}
+
+	}
+
+	return nil
+}
