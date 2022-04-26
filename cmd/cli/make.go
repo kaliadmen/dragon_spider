73d888f0340dcc40e4a81ad7ff17afd07191c812
kaliadmen <kaliadmen@gmail.com>
2022-04-26 17:52:08 -0400

    ADDS MAKE HANDLER

--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -3,12 +3,43 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/iancoleman/strcase"
+	"io/ioutil"
 	"strings"
 	"time"
 )
 
 func runMake(arg2, arg3 string) error {
 	switch strings.ToLower(arg2) {
+	case "auth":
+		err := runAuth()
+		if err != nil {
+			gracefulExit(err)
+		}
+
+	case "handler":
+		if arg3 == "" {
+			gracefulExit(errors.New("handler must have a name"))
+		}
+
+		fileName := ds.RootPath + "/handlers/" + strings.ToLower(arg3) + ".go"
+		if fileExists(fileName) {
+			gracefulExit(errors.New(fileName + "already exist"))
+		}
+
+		data, err := templateFs.ReadFile("templates/handlers/handler.go.txt")
+		if err != nil {
+			gracefulExit(err)
+		}
+
+		handler := string(data)
+		handler = strings.ReplaceAll(handler, "$HANDLERNAME$", strcase.ToCamel(strings.ToLower(arg3)))
+
+		err = ioutil.WriteFile(fileName, []byte(handler), 0644)
+		if err != nil {
+			gracefulExit(err)
+		}
+
 	case "migration":
 		dbType := ds.Db.DatabaseType
 		if arg3 == "" {
@@ -26,12 +57,6 @@ func runMake(arg2, arg3 string) error {
 		}
 
 		err = makeFileFromTemplate("templates/migrations/migration."+dbType+".down.sql", downFile)
-		if err != nil {
-			gracefulExit(err)
-		}
-
-	case "auth":
-		err := runAuth()
 		if err != nil {
 			gracefulExit(err)
 		}
