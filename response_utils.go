cff7b153105366d594104c120e55acd159893681
kaliadmen <kaliadmen@gmail.com>
2022-04-26 00:10:55 -0400

    ADDS JSON RESPONSE FUNCTION

---
+++ response_utils.go
@@ -0,0 +1,28 @@
+package dragonSpider
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (ds *DragonSpider) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
