aa7e25a0a9361350194ed6d62fc58dd13d88c20d
kaliadmen <kaliadmen@gmail.com>
2022-04-28 18:24:31 -0400

    fixup! ADDS MAKE HANDLER FILE

---
+++ cmd/cli/handler.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"github.com/iancoleman/strcase"
+	"io/ioutil"
+	"strings"
+)
+
+func makeHandler(name string) error {
+	fileName := ds.RootPath + "/handlers/" + strings.ToLower(name) + ".go"
+	if fileExists(fileName) {
+		gracefulExit(errors.New(fileName + "already exist"))
+	}
+
+	data, err := templateFs.ReadFile("templates/handlers/handler.go.txt")
+	if err != nil {
+		gracefulExit(err)
+	}
+
+	handler := string(data)
+	handler = strings.ReplaceAll(handler, "$HANDLERNAME$", strcase.ToCamel(strings.ToLower(name)))
+
+	err = ioutil.WriteFile(fileName, []byte(handler), 0644)
+	if err != nil {
+		gracefulExit(err)
+	}
+
+	return nil
+}
