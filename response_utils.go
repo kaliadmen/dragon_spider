1e0ca9cd354ee18f94cfb017478457972f0ff72d
kaliadmen <kaliadmen@gmail.com>
2022-05-04 10:43:31 -0400

    ADDS MORE RESPONSE UTILITIES

--- response_utils.go
+++ response_utils.go
@@ -2,7 +2,11 @@ package dragonSpider
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
+	"path/filepath"
 )
 
 func (ds *DragonSpider) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -25,4 +29,54 @@ func (ds *DragonSpider) WriteJSON(w http.ResponseWriter, status int, data interf
 	}
 
 	return nil
+}
+
+func (ds *DragonSpider) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ds *DragonSpider) FileDownload(w http.ResponseWriter, r *http.Request, pathToFile, filename string) error {
+	fp := path.Join(pathToFile, filename)
+	fileToServer := filepath.Clean(fp)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
+	http.ServeFile(w, r, fileToServer)
+	return nil
+}
+
+func (ds *DragonSpider) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (ds *DragonSpider) Error401(w http.ResponseWriter, r *http.Request) {
+	ds.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+func (ds *DragonSpider) Error403(w http.ResponseWriter, r *http.Request) {
+	ds.ErrorStatus(w, http.StatusForbidden)
+}
+
+func (ds *DragonSpider) Error404(w http.ResponseWriter, r *http.Request) {
+	ds.ErrorStatus(w, http.StatusNotFound)
+}
+
+func (ds *DragonSpider) Error500(w http.ResponseWriter, r *http.Request) {
+	ds.ErrorStatus(w, http.StatusInternalServerError)
 }
