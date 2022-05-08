90795fb918848b11127d28084fda21ef3f52df78
kaliadmen <kaliadmen@gmail.com>
2022-05-08 12:14:35 -0400

    ADDS BADGER DB CACHE

---
+++ cache/badger_cache.go
@@ -0,0 +1,155 @@
+package cache
+
+import (
+	"errors"
+	"github.com/dgraph-io/badger/v3"
+	"time"
+)
+
+type BadgerCache struct {
+	Connection *badger.DB
+	Prefix     string
+}
+
+func (bc *BadgerCache) Has(key string) (bool, error) {
+	_, err := bc.Get(key)
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (bc *BadgerCache) Get(key string) (interface{}, error) {
+	var fromCache []byte
+
+	err := bc.Connection.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+
+		err = item.Value(func(val []byte) error {
+			fromCache = append([]byte{}, val...)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	decoded, err := decode(string(fromCache))
+	if err != nil {
+		return nil, err
+	}
+
+	item := decoded[key]
+
+	return item, nil
+}
+
+func (bc *BadgerCache) Set(key string, value interface{}, expires ...int) error {
+	if key == "" || value == "" {
+		return errors.New("blank entries are not allowed")
+	}
+
+	entry := Entry{}
+
+	entry[key] = value
+	encoded, err := encode(entry)
+	if err != nil {
+		return err
+	}
+
+	if len(expires) > 0 {
+		err = bc.Connection.Update(func(txn *badger.Txn) error {
+			e := badger.NewEntry([]byte(key), encoded).WithTTL(time.Second * time.Duration(expires[0]))
+			err = txn.SetEntry(e)
+			return err
+		})
+	} else {
+		err = bc.Connection.Update(func(txn *badger.Txn) error {
+			e := badger.NewEntry([]byte(key), encoded)
+			err = txn.SetEntry(e)
+			return err
+		})
+	}
+
+	return nil
+}
+
+func (bc *BadgerCache) Delete(key string) error {
+	err := bc.Connection.Update(func(txn *badger.Txn) error {
+		err := txn.Delete([]byte(key))
+		return err
+	})
+
+	return err
+}
+
+func (bc *BadgerCache) DeleteByMatch(key string) error {
+
+	return bc.deleteByMatch(key)
+}
+
+func (bc *BadgerCache) DeleteAll() error {
+
+	return bc.deleteByMatch("")
+}
+
+func (bc *BadgerCache) deleteByMatch(key string) error {
+	//search cache for keys to be deleted
+	deletedKeys := func(keysToDelete [][]byte) error {
+		if err := bc.Connection.Update(func(txn *badger.Txn) error {
+			for _, k := range keysToDelete {
+				if err := txn.Delete(k); err != nil {
+					return err
+				}
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	//limit how much cache is used at one time
+	collectionSize := 100000
+
+	err := bc.Connection.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.AllVersions = false
+		opts.PrefetchValues = false
+		i := txn.NewIterator(opts)
+
+		defer i.Close()
+
+		keysToDelete := make([][]byte, 0, collectionSize)
+		keysCollected := 0
+
+		for i.Seek([]byte(key)); i.ValidForPrefix([]byte(key)); i.Next() {
+			k := i.Item().KeyCopy(nil)
+			keysToDelete = append(keysToDelete, k)
+			keysCollected++
+			if keysCollected == collectionSize {
+				if err := deletedKeys(keysToDelete); err != nil {
+					return err
+				}
+			}
+		}
+		if keysCollected > 0 {
+			if err := deletedKeys(keysToDelete); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return err
+
+}
--- go.mod
+++ go.mod
@@ -23,10 +23,21 @@ require (
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
 	github.com/alicebob/miniredis/v2 v2.21.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgraph-io/badger/v3 v3.2103.2 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/gertd/go-pluralize v0.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gomodule/redigo v1.8.8 // indirect
+	github.com/google/flatbuffers v2.0.0+incompatible // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
@@ -38,19 +49,24 @@ require (
 	github.com/jackc/pgtype v1.10.0 // indirect
 	github.com/justinas/nosurf v1.1.1 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	github.com/upper/db/v4 v4.5.2 // indirect
 	github.com/yuin/gopher-lua v0.0.0-20210529063254-f4c35e4016d9 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b // indirect
 	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	modernc.org/cc/v3 v3.32.4 // indirect
 	modernc.org/ccgo/v3 v3.9.2 // indirect
 	modernc.org/libc v1.9.5 // indirect
