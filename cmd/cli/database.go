a33d66603ce49b56842459a61e3c8b8d01db6219
kaliadmen <kaliadmen@gmail.com>
2022-05-20 13:10:19 -0400

    MODIFIES MAKE DATABASE FUNCTION

--- cmd/cli/database.go
+++ cmd/cli/database.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func makeSqliteDb() error {
-	err := ds.CreateDir(ds.RootPath + "/tmp/sqlite")
+	err := ds.CreateDirs(ds.RootPath + "/db/sqlite")
 	if err != nil {
 		return err
 	}
-	if !fileExists(ds.RootPath + "tmp/sqlite/app.db") {
-		err := ds.CreateFile(fmt.Sprintf("%s/tmp/sqlite/app.db", ds.RootPath))
+
+	if !fileExists(ds.RootPath + "db/sqlite/app.db") {
+		err := ds.CreateFile(fmt.Sprintf("%s/db/sqlite/app.db", ds.RootPath))
 		if err != nil {
 			return err
 		}
