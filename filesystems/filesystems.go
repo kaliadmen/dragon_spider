df86c49ec5994b84ac0be4e4081f23e6e15c6376
kaliadmen <kaliadmen@gmail.com>
2022-06-07 15:30:09 -0400

    ADD FILESYSTEMS PACKAGE

---
+++ filesystems/filesystems.go
@@ -0,0 +1,21 @@
+package filesystems
+
+import "time"
+
+//Fs is an interface for filesystems.
+//All functions in this interface must exist in order to be implemented.
+type Fs interface {
+	Put(fileName, directory string) error
+	Get(destination string, items ...string) error
+	List(prefix string) ([]Listing, error)
+	Delete(itemsToDelete ...string) bool
+}
+
+//Listing represents a file on a remote file system
+type Listing struct {
+	Etag         string
+	LastModified time.Time
+	Key          string
+	Size         float64
+	IsDir        bool
+}
