7288f0abd4d2f4717958fb6aeb42c0ad3a623e25
kaliadmen <kaliadmen@gmail.com>
2022-06-20 15:24:53 -0400

    Revert "Revert "ADD REFACTOR OF BADGER DB FILE LOCATION""
    This reverts commit 0594b1f7ba03e7d3bbd13b4b919ab762177a3e97.

--- dragon_spider.go
+++ dragon_spider.go
@@ -342,7 +342,7 @@ func (ds *DragonSpider) createRedisPool() *redis.Pool {
 //createBadgerConnection creates a new connection to badger db
 func (ds *DragonSpider) createBadgerConnection() (*badger.DB, error) {
 	if ds.Debug {
-		db, err := badger.Open(badger.DefaultOptions(ds.RootPath + "/tmp/badger"))
+		db, err := badger.Open(badger.DefaultOptions(ds.RootPath + "/db/badger"))
 		if err != nil {
 			return nil, err
 		}
@@ -350,7 +350,7 @@ func (ds *DragonSpider) createBadgerConnection() (*badger.DB, error) {
 		return db, nil
 	}
 
-	db, err := badger.Open(badger.DefaultOptions(ds.RootPath + "/tmp/badger").WithLogger(nil))
+	db, err := badger.Open(badger.DefaultOptions(ds.RootPath + "/db/badger").WithLogger(nil))
 	if err != nil {
 		return nil, err
 	}
